43e5a278f8737211b31578a8e723084e5d471f43
createitv <xfy150150@163.com>
2023-03-09 23:30:14 +0800

    add random and random_test.go

---
+++ random/random.go
@@ -0,0 +1,107 @@
+// Copyright 2023 xfy150150@gmail.com. All rights reserved.
+// @Time        : 2023/3/9 21:42
+// @Author      : Createitv
+// @FileName    : random.go
+// @Software    : GoLand
+// @WeChat      : Navalism1
+// @Description : 
+
+// Package random implements some function to generate random Alphabet or EnglishString
+package random
+
+import (
+	craned "crypto/rand"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+const (
+	ArabicNumerals = "0123456789"
+	LowerCharacter = "abcdefghijklmnopqrstuvwxyz"
+	UpperCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	Alphabet       = LowerCharacter + UpperCharacter
+	EnglishString  = Alphabet + ArabicNumerals
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandBetween 生成min到max之间的随机数，包括min不包括max
+func RandBetween(min, max int) int {
+	if max == min {
+		return min
+	}
+	if min > max {
+		min, max = max, min
+	}
+	return rand.Intn(max-min) + min
+}
+
+// RandBytes 生成 length 长度的字节切片
+func RandBytes(length int) []byte {
+	if length < 1 {
+		return []byte{}
+	}
+	b := make([]byte, length)
+	if _, err := io.ReadFull(craned.Reader, b); err != nil {
+		return nil
+	}
+	
+	return b
+}
+
+// RandFromString 从一段给定字符串中生成一定长度的字符串
+func RandFromString(s string, length int) string {
+	b := make([]byte, length)
+	
+	for i := range b {
+		b[i] = s[rand.Int63()%int64(len(s))]
+	}
+	
+	return string(b)
+}
+
+// RandNumber 生成 length 长度的只有数字的字符串
+func RandNumber(length int) string {
+	return RandFromString(ArabicNumerals, length)
+}
+
+// RandString 生成 length 长度的只有52个大小写英文字母的字符串
+func RandString(length int) string {
+	return RandFromString(Alphabet, length)
+}
+
+// RandUpperString 生成 length 长度的只有26个大写英文字母的字符串
+func RandUpperString(length int) string {
+	return RandFromString(UpperCharacter, length)
+}
+
+// RandLowerString 生成 length 长度的只有26个小写英文字母的字符串
+func RandLowerString(length int) string {
+	return RandFromString(LowerCharacter, length)
+}
+
+// RandEnglishString 生成 length 长度的只有52个大小写英文字母 + 数字的字符串
+func RandEnglishString(length int) string {
+	return RandFromString(EnglishString, length)
+}
+
+// UUIdV4 生成依据RFC 4122 的UUID
+func UUIdV4() (string, error) {
+	uuid := make([]byte, 16)
+	
+	n, err := io.ReadFull(craned.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10],
+		uuid[10:],
+	), nil
+}
---
+++ random/random_test.go
@@ -0,0 +1,98 @@
+// Copyright 2023 xfy150150@gmail.com. All rights reserved.
+// @Time        : 2023/3/9 22:17
+// @Author      : Createitv
+// @FileName    : random_test.go.go
+// @Software    : GoLand
+// @WeChat      : Navalism1
+// @Description : 
+
+package random
+
+import (
+	"reflect"
+	"regexp"
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUUIdV4(t *testing.T) {
+	
+	uuid, err := UUIdV4()
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	isUUiDV4 := regexp.MustCompile(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$`)
+	assert.Equal(t, true, isUUiDV4.MatchString(uuid), "uuid 正则格式需要相匹配")
+}
+
+func TestRandBetween(t *testing.T) {
+	r1 := RandBetween(1, 10)
+	assert.GreaterOrEqual(t, r1, 1)
+	assert.Less(t, r1, 10)
+	
+	r2 := RandBetween(1, 1)
+	assert.Equal(t, 1, r2)
+	
+	r3 := RandBetween(10, 1)
+	assert.GreaterOrEqual(t, r1, 1)
+	assert.Less(t, r3, 10)
+}
+
+func TestRandFromString(t *testing.T) {
+	c := RandFromString(Alphabet, 10)
+	for i := 0; i < len(c); i++ {
+		assert.Contains(t, Alphabet, string(c[i]))
+	}
+	
+	s := "213sadkasj209m231-a."
+	z := RandFromString(s, 10)
+	for i := 0; i < len(c); i++ {
+		assert.Contains(t, s, string(z[i]))
+	}
+}
+
+func TestRandNumber(t *testing.T) {
+	r := RandNumber(10)
+	for i := 0; i < len(r); i++ {
+		assert.Contains(t, ArabicNumerals, string(r[i]))
+	}
+}
+
+func TestRandString(t *testing.T) {
+	r := RandString(10)
+	for i := 0; i < len(r); i++ {
+		assert.Contains(t, Alphabet, string(r[i]))
+	}
+}
+
+func TestRandUpperString(t *testing.T) {
+	r := RandUpperString(10)
+	for i := 0; i < len(r); i++ {
+		assert.Contains(t, UpperCharacter, string(r[i]))
+	}
+}
+
+func TestRandLowerString(t *testing.T) {
+	r := RandLowerString(10)
+	for i := 0; i < len(r); i++ {
+		assert.Contains(t, LowerCharacter, string(r[i]))
+	}
+}
+
+func TestRandEnglishString(t *testing.T) {
+	r := RandLowerString(10)
+	for i := 0; i < len(r); i++ {
+		assert.Contains(t, EnglishString, string(r[i]))
+	}
+}
+
+func TestRandBytes(t *testing.T) {
+	randBytes := RandBytes(4)
+	assert.Equal(t, 4, len(randBytes), "random bytes length must be equal to 4")
+	assert.Equal(t, []byte{}, RandBytes(0),
+		"null bytes slice must be equal to []byte{}",
+	)
+	assert.Equal(t, reflect.Slice, reflect.ValueOf(randBytes).Kind())
+}
